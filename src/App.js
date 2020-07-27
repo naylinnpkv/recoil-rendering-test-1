9faf211f6b31a8d06e3e51e766279ff20d5d1392
naylinnpkv <kathir.pkv@gmail.com>
2020-07-27 09:35:41 -0400

    pure react, can't pass down the names, but functions been passed. Check console.log

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumComponent />
+    </div>
+  );
+}
+
+function NumComponent() {
+  const [firstNum, setFirst] = useState({ firstNum: 2 });
+  const [secondNum, setSecond] = useState({
+    secondNum: 4,
+  });
+  const [state, setState] = useState({
+    firstNumChange: { firstNumChange },
+    secondNumChange: { secondNumChange },
+  });
+
+  function firstNumChange(e) {
+    setFirst(e.target.value);
+  }
+  function secondNumChange(e) {
+    setSecond(e.target.value);
+  }
+  return (
+    <section>
+      <SignComponent
+        firstNumChange={firstNumChange}
+        secondNumChange={secondNumChange}
+        firstNum={firstNum.firstNum}
+        secondNum={secondNum.secondNum}
+      />
+      <input value={firstNum} onChange={firstNumChange} />
+      <input value={secondNum} onChange={secondNumChange} />
+    </section>
+  );
+}
+
+function SignComponent(props) {
+  const [count, setCount] = useState("+");
+
+  function valueChange() {
+    setCount(props.firstNum + props.secondNum);
+  }
+  console.log(props.firstNum, "firstNum,secondNum");
+  return (
+    <div>
+      <button class="btn" onClick={valueChange}>
+        {count}
+      </button>
     </div>
   );
 }
